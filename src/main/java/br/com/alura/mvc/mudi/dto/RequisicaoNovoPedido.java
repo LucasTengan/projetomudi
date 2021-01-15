ab46ca4685ecd2a194277a2b49ea439380c298fc
Lucas Tengan <lucastengan@usp.br>
2021-01-15 01:04:33 -0300

    Criando o formulário para criar um pedido, enviando os dados para o backend e salvando no banco de dados com Spring Data

---
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,54 @@
+package br.com.alura.mvc.mudi.dto;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+	private String nomeProduto;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setDescricao(descricao);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		
+		return pedido;
+	}
+
+}
